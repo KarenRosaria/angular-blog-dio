5eca5d7e22e35172de778f1068002f4b72a67d3a
karen <karenrosaria33@gmail.com>
2024-02-20 23:08:51 -0300

    criação de novas páginas e mudança do ícone

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,33 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Oppenheimer vence o BAFTA 2024",
+    "description": "Entre os troféus vencidos pelo longa-metragem, categorias de ampla popularidade como Melhor Filme, Melhor Direção, Melhor Ator e Melhor Ator Coadjuvante foram levadas para casa. A cinebiografia do físico norte-americano ao todo arrematou 7 estatuetas, seguido de perto por Pobres Criaturas, vencedor em 5 categorias.",
+    "photoCover":"https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2023/07/Oppenheimer-3.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Ferrari",
+    "description": "Estréia 22 de fevereiro  |2h 10min  | Cinebiografia  - Ferrari é uma cinebiografia que conta a história de vida do poderoso empresário italiano de carros esportivos Enzo Ferrari (interpretado por Adam Driver). O filme mostra como Ferrari e sua família revolucionaram a indústria automotiva e, de certa forma, ajudaram a criar o conceito das corridas de Fórmula 1. Selecionado para o Festival de Veneza de 2023, dirigido por Michael Mann (O Informante, O Último dos Moicanos) e estrelado por, além de Driver, Shailene Woodley, Sarah Gadon, Penélope Cruz, Jack O'Connell, Patrick Dempsey e o brasileiro Gabriel Leone.",
+    "photoCover":"https://cdn.motor1.com/images/mgl/xqb62y/s1/film-ferrari.webp"
+  },
+  {
+    "id":"3",
+    "title": "Zona de Risco",
+    "description": "Estréia 22 de fevereiro  |1h 50min  | Ação, Suspence  - Em Land of Bad, um piloto de drone das Forças Aéreas é a única esperança de uma força-tarefa presa em uma emboscada. No thriller de ação, a missão de um esquadrão de operações especiais da Força Delta vira um grande desastre. Cercados pelo inimigo em um território remoto nas Filipinas, a única chance de resgate está nas mãos de Reaper (Russel Crowe), um piloto de drone, e Kinney (Liam Hemsworth), um jovem oficial da aeronáutica. Eles têm 48 horas para resgatar os soldados antes que vire um banho de sangue.",
+    "photoCover":"https://cinepop.com.br/wp-content/uploads/2024/01/zona-de-risco.jpg.webp"
+  },
+  {
+    "id":"4",
+    "title": "Nosso Verão Daria Um Filme",
+    "description": "Estréia 22 de fevereiro  |1h 46min  | Comédia, Romance  - Os melhores amigos Demos (Yorgos Tsiantoulas) e Nikitas (Andreas Labropoulos) aproveitam juntos os próximos dias ensolarados nas águas de uma praia em Atenas. Juntos, eles acabam recordando as memórias inesquecíveis do último verão. Durante esse tempo, as lembranças vão ganhando vida sob a perspectiva cinematográfica ao ponto de transformá-las em um roteiro para o primeiro longa-metragem de Nikitas. Mas, ao revisitar antigos momentos como se fosse um filme, as desavenças que estavam pendentes surgem, abalando a amizade colorida entre eles.",
+    "photoCover":"https://cinepop.com.br/wp-content/uploads/2023/12/nosso-verao1-1024x576.jpg.webp"
+  },
+
+  {
+    "id":"5",
+    "title": "Ingresso de cimena por R$ 12,00",
+    "description": "Esta é a terceira vez que a ação acontece no país. Idealizada pela Federação Nacional das Empresas Exibidoras Cinematográficas (FENEEC) em parceria com a Associação Brasileira das Empresas Exibidoras Cinematográficas Operadoras de Multiplex (ABRAPLEX), a Semana do Cinema é uma iniciativa feita justamente para atrair o público e democratizar o acesso à arte.",
+    "photoCover":"https://f.i.uol.com.br/fotografia/2022/09/15/16632561726323466c87406_1663256172_3x2_rt.jpg"
   }
 ]
